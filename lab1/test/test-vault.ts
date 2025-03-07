c0c5e13466f79475bc39b78a0215c9c84a66328d
flyotlin <flyotlin@gmail.com>
2025-03-07 23:53:06 +0800

    feat: lab1 tests

--- lab1/test/test-vault.ts
+++ lab1/test/test-vault.ts
@@ -3,22 +3,35 @@ import { expect } from 'chai'
 
 
 describe('Vault', function () {
-    it('should receive eth and emit certain event', async function () {
+    it('should receive correct amount of eth', async function () {
         let deployer, heir
         [deployer, heir] = await hre.ethers.getSigners()
-
 
         let vault = await hre.ethers.deployContract('Vault', [heir])
         const vaultAddress = await vault.getAddress()
 
         const vaultBalanceBefore = await hre.ethers.provider.getBalance(vaultAddress)
+        await deployer.sendTransaction({
+            to: vault.getAddress(),
+            value: hre.ethers.parseEther('1')
+        })
+
+        const vaultBalanceAfter = await hre.ethers.provider.getBalance(vaultAddress)
+        // assert address has xxx value
+        expect(vaultBalanceAfter - vaultBalanceBefore).to.be.equal(await hre.ethers.parseEther('1'))
+    })
+
+    it('should emit event with address and the amount', async function () {
+        let deployer, heir
+        [deployer, heir] = await hre.ethers.getSigners()
+
+        let vault = await hre.ethers.deployContract('Vault', [heir])
+
         const tx = await deployer.sendTransaction({
             to: vault.getAddress(),
             value: hre.ethers.parseEther('1')
         })
         const receipt = await tx.wait()
-
-        const vaultBalanceAfter = await hre.ethers.provider.getBalance(vaultAddress)
 
         // assert event with address and value
         const log = receipt?.logs[0]
@@ -32,20 +45,37 @@ describe('Vault', function () {
         expect(parsedLog?.name).to.equal("DepositEth")
         expect(parsedLog?.args.sender).to.equal(deployer.address)
         expect(parsedLog?.args.amount).to.equal(hre.ethers.parseEther('1'))
-
-
-        // assert address has xxx value
-        expect(vaultBalanceAfter - vaultBalanceBefore).to.be.equal(await hre.ethers.parseEther('1'))
     })
 
-    // it('should emit event with address and the amount', function () {
+    it('should be withdrawed by only one specific address', async function () {
+        let deployer, heir
+        [deployer, heir] = await hre.ethers.getSigners()
 
-    // })
+        const balanceOriginal = await hre.ethers.provider.getBalance(heir.address)
 
-    // it('should be withdrawed all money by only one specific address', function () {
-    //     await deployer.sendTransaction({
-    //         to: vault.getAddress(),
-    //         value: hre.ethers.parseEther('1')
-    //     })
-    // })
+        let vault = await hre.ethers.deployContract('Vault', [heir])
+
+        const tx = await deployer.sendTransaction({
+            to: vault.getAddress(),
+            value: hre.ethers.parseEther('1')
+        })
+
+        // as of now, there's 1 ether in the vault
+        const v = await vault.connect(heir)
+        await v.withdraw()
+
+        const receipt = await tx.wait()
+        const gasUsed = receipt.gasUsed
+        const gasPrice = receipt.gasPrice
+
+        const balanceAfter = await hre.ethers.provider.getBalance(heir.address)
+
+        const expectedIncrease = hre.ethers.parseEther('1')  // vault中的ETH
+        const actualIncrease = balanceAfter - balanceOriginal
+
+        // 扣掉gas fee之後的增加量
+        const expectedNetIncrease = expectedIncrease - gasUsed * gasPrice
+
+        expect(actualIncrease).to.be.approximately(expectedNetIncrease, 100000000000000)
+    })
 })
